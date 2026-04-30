a144bbc3820381f3844d40681dcdbe7ab695f6d9
Yara Essam Ellkany <117939379+Yara-Ellkany@users.noreply.github.com>
2026-04-30 14:46:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from transformers import pipeline
  
-st.title(" Summarizer ")
-st.write("اكتب اي نص تريده!")
- 
+st.title("Summarize")
+
+#يحمل النموذج مره واحده فقط
 @st.cache_resource
 def load_model():
-    return pipeline("summarization", model="facebook/bart-large-cnn")
+    return pipeline("text2text-generation", model="marefa-nlp/summarization-arabic-english-news")
  
 summarizer = load_model()
  
@@ -14,8 +14,8 @@ text = st.text_area("اكتب النص هنا...", height=200)
  
 if st.button(" لخّص!"):
     if len(text.split()) < 30:
-        st.warning("⚠️ النص قصير جداً! اكتب 30 كلمة على الأقل.")
+        st.warning("النص قصير جداً! اكتب 30 كلمة على الأقل.")
     else:
         with st.spinner("جاري التلخيص..."):
-            result = summarizer(text, max_length=80, min_length=30, do_sample=False)
-            st.success(result[0]["summary_text"])
+            result = summarizer(text, max_length=120, min_length=30, do_sample=False)
+            st.success(result[0]["generated_text"])
